0451e9ce8a9b0db9aff0e2dd9a82f8813cba14de
moustafa ascoura <moustafaascoura@gmail.com>
2023-09-30 21:06:01 +0300

    update routes

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -17,6 +17,6 @@ export default function Router () {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Home />} /*element={<NotFound />}*//> 
     </Routes>)
 }
